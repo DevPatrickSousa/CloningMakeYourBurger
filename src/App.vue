f9464ef20fbba84c358f1160ecbfbda1fefd3471
DevPatrickSousa <patrickseven22@hotmail.com>
2022-06-07 15:20:21 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Nav-Bar :logo="logo_src" :alt="app_name" />
+  <router-view />
+  <Footer />
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    NavBar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "img/logo.png",
+      app_name: "make your burger",
+    };
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <router-link to="/" id="logo_props">
+      <img :src="logo" :alt="alt" id="logo" />
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  name: "NavBar",
+  props: ["logo", "alt"],
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  justify-content: flex-end;
+  align-content: center;
+}
+#logo_props{
+  margin: auto;
+  margin-left: 0;
+}
+#logo{
+  width: 40px;
+  height: 40px;
+
+}
+nav a{
+  text-decoration: none;
+  color: #FCBA03;
+  margin: 12px;
+  transition: .5s;
+}
+
+nav a:hover{
+  color: #FFFF;
+}
+</style>
